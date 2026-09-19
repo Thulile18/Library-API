e9ef7aed1e987460467da9a73c1530be8a26c216
Thulile18 <busisiwegqada0@gmail.com>
2026-09-19 19:09:39 +0200

    Implemented author routes with CRUD operations

--- src/src/routes/authorRoutes.ts
+++ src/src/routes/authorRoutes.ts
@@ -1,1 +1,14 @@
+import express = require("express");
+import authorController = require("../controllers/authorController");
+import validate = require("../middleware/validate");
 
+const router = express.Router();
+
+router.post("/", validate.validateAuthor, authorController.createAuthor);
+router.get("/", authorController.getAuthors);
+router.get("/:id", authorController.getAuthorById);
+router.put("/:id", validate.validateAuthor, authorController.updateAuthor);
+router.delete("/:id", authorController.deleteAuthor);
+router.get("/:id/books", authorController.getBooksByAuthor);
+
+export = router;
