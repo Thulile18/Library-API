ac2db6083565798b59e8e532c6799ebc610c900c
Thulile18 <busisiwegqada0@gmail.com>
2026-09-19 19:17:02 +0200

    Added Author and Book interfaces to types.ts

--- src/src/types.ts
+++ src/src/types.ts
@@ -1,1 +1,14 @@
+export interface Author {
+  id: number;
+  name: string;
+  bio?: string;
+  birthYear?: number;
+}
 
+export interface Book {
+  id: number;
+  title: string;
+  authorId: number;
+  year: number;
+  isbn?: string;
+}
