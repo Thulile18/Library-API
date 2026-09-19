138cabc1c9e7c650b4c954ea00cf4fd71ed35eff
Thulile18 <busisiwegqada0@gmail.com>
2026-09-19 18:32:41 +0200

    Created and added HttpError class for handling HTTP errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,1 +1,23 @@
+class HttpError extends Error {
+  status: number;
+  details: string[] | undefined;
+
+  constructor(status: number, message: string, details?: string[]) {
+    super(message);
+    this.status = status;
+    this.details = details;
+  }
+
+  static badRequest(message: string, details?: string[]) {
+    return new HttpError(400, message, details);
+  }
+  static notFound(message: string) {
+    return new HttpError(404, message);
+  }
+  static conflict(message: string) {
+    return new HttpError(409, message);
+  }
+}
+
+export = HttpError;
 
