901e3c0bfdb31f4ca586487d28501532808a218f
Thulile18 <busisiwegqada0@gmail.com>
2026-09-19 19:11:34 +0200

    Implemented book routes with CRUD operations

--- src/src/routes/bookRoutes.ts
+++ src/src/routes/bookRoutes.ts
@@ -1,1 +1,13 @@
+import express = require("express");
+import bookController = require("../controllers/bookController");
+import validate = require("../middleware/validate");
 
+const router = express.Router();
+
+router.post("/", validate.validateBook, bookController.createBook);
+router.get("/", bookController.getBooks);
+router.get("/:id", bookController.getBookById);
+router.put("/:id", validate.validateBook, bookController.updateBook);
+router.delete("/:id", bookController.deleteBook);
+
+export = router;
